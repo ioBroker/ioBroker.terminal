a62146e2e46bbd1183a91f0a5c76b31b44dfe576
Ingo Fischer <github@fischer-ka.de>
2019-03-03 22:45:44 +0100

    add adapter-core

--- main.js
+++ main.js
@@ -5,7 +5,7 @@
 var express =  require('express');
 var fs =       require('fs');
 var Stream =   require('stream');
-var utils =    require(__dirname + '/lib/utils'); // Get common adapter utils
+var utils = require('@iobroker/adapter-core'); // Get common adapter utils
 var terminal = require(__dirname + '/lib/web-terminal');
 var path =     require('path');
 
